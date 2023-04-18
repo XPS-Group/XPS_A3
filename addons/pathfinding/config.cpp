77551fe9012c9e537b8bafa65bb1ca63491d6338
Crashdome <110640480+CrashDome@users.noreply.github.com>
2023-04-17 22:00:19 -0500

    Init

---
+++ addons/pathfinding/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgMods {
+    class ADDON {
+        name = "XPS Pathfinding";
+        dir = COMPONENT_DIR;
+        author = "CrashDome";
+
+        description = "Extensible Project System - Pathfinding";
+
+        overview = "Work In Progress";
+    };
+};
+
+#include "CfgPatches.hpp"
+#include "CfgFunctions.hpp"
+#include "CfgEventhandlers.hpp"
---
+++ addons/pathfinding/script_component.hpp
@@ -0,0 +1,15 @@
+#define COMPONENT pf
+#include "\x\xps\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_CACHE_DISABLE
+	#define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_CACHE_DISABLE
+	#define DEBUG_SETTINGS DEBUG_SETTINGS_CACHE_DISABLE
+#endif
+
+// #undef REQUIRED_VERSION
+// #define REQUIRED_VERSION 1.00
+
+#include "script_macros.hpp"
