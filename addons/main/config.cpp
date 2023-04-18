f655d418891a001fa1cd61922e864e8c88830dfd
Crashdome <110640480+CrashDome@users.noreply.github.com>
2023-04-17 21:55:35 -0500

    Init

---
+++ addons/main/config.cpp
@@ -0,0 +1,36 @@
+#include "script_component.hpp"
+
+class CfgMods {
+    class ADDON {
+        name = "XPS - Extensible Project System";
+        dir = COMPONENT_DIR;
+        author = "CrashDome";
+
+        description = "Extensible Project System - Core Functionality";
+
+        overview = "Work In Progress";
+    };
+};
+
+class CfgPatches {
+	class ADDON {
+			// Meta information for editor
+			name = CSTRING(component);
+			author = "CrashDome";
+			url = "";
+	
+			// Minimum compatible version. When the game's version is lower, pop-up warning will appear when launching the game.
+			requiredVersion = REQUIRED_VERSION; 
+			// Required addons, used for setting load order.
+			// When any of the addons is missing, pop-up warning will appear when launching the game.
+			requiredAddons[] = { "cba_main" };
+        	VERSION_CONFIG;
+			// List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
+			units[] = {};
+			// List of weapons (CfgWeapons classes) contained in the addon.
+			weapons[] = {};
+	};
+};
+
+#include "CfgFunctions.hpp"
+#include "CfgEventhandlers.hpp"
