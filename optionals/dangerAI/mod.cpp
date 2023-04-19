14d7752afbe1d794c6726f4cd9ffa400426ef8d9
Crashdome <110640480+CrashDome@users.noreply.github.com>
2023-04-18 21:12:50 -0500

    Init

---
+++ optionals/dangerAI/mod.cpp
@@ -0,0 +1,6 @@
+name = "XPS Danger AI";
+tooltip = "eXtensible Project System - Danger AI";
+tooltipOwned = "eXtensible Project System Owned";
+overview = "Work In Progress";
+author = "XPS Team";
+overviewText = "eXtensible Project System overviewText";
---
+++ optionals/formationAI/script_macros.hpp
@@ -0,0 +1,2 @@
+#include "\x\xps\addons\main\script_macros.hpp"
+#include "\x\xps\addons\bt\script_macros.hpp"
