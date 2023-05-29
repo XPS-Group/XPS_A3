caab1af7b2f192fac15f4fdda0f35ca35463d3e3
Crashdome <110640480+CrashDome@users.noreply.github.com>
2023-05-29 15:43:05 -0500

    init

--- optionals/formationAI/CfgVehicles.hpp
+++ optionals/formationAI/CfgVehicles.hpp
@@ -1,7 +1,6 @@
 class CfgVehicles {
 	class Man;
 	class CAManBase : Man {
-		//fsmFormation = "x\xps\addons\f\fsms\xps_f_bt_formation.fsm";
-		//fsmFormation = "x\xps\addons\f\fsms\xps_f_bt_formation_dev.fsm";
+		fsmFormation = "x\xps\addons\f\fsms\formation.fsm";
 	};
 };
