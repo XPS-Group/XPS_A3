675764854a88da96f76f206df629d001a15c66cf
Crashdome <110640480+CrashDome@users.noreply.github.com>
2023-06-05 19:39:42 -0500

    reclaim test objects

--- addons/main/CfgTypeDefinitions.hpp
+++ addons/main/CfgTypeDefinitions.hpp
@@ -6,11 +6,14 @@ class XPS_CFG_BASECLASSNAME {
 		XPS_CFG_IFC(IBlackboard);
 		XPS_CFG_IFC(ICollection);
 		XPS_CFG_IFC(IJobScheduler);
+		XPS_CFG_IFC(ITestObject);
 
 		XPS_CFG_TYP(AstarSearch);
 		XPS_CFG_TYP(Blackboard);
 		XPS_CFG_TYP(Collection);
 		XPS_CFG_TYP(HashmapCollection);
 		XPS_CFG_TYP(JobScheduler);
+		XPS_CFG_TYP(TestObjectA);
+		XPS_CFG_TYP(TestObjectB);
 	};
 };
